342226c20359043849935884c389b7083828adc7
Volodya <andrij.chypra@gmail.com>
2024-02-07 17:01:02 +0200

    Set up project

---
+++ src/main/java/org/eventhub/main/EventhubBackendApplication.java
@@ -0,0 +1,13 @@
+package org.eventhub.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhubBackendApplication.class, args);
+	}
+
+}
