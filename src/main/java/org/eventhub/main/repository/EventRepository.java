f2a2338e91063290f8dfb5ac9d3bac798ab13bc3
= <ssaputskiy@gmail.com>
2024-02-08 12:00:52 +0200

    Event repository complete

---
+++ src/main/java/org/eventhub/main/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package org.eventhub.main.repository;
+
+import org.eventhub.main.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
