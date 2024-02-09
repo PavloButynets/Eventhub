604eee8cac1d475374e32fbdefa3772a8268bce6
= <ssaputskiy@gmail.com>
2024-02-09 17:13:50 +0200

    Added NotValidDateException and create method in servises for event

--- src/main/java/org/eventhub/main/service/EventService.java
+++ src/main/java/org/eventhub/main/service/EventService.java
@@ -1,11 +1,12 @@
 package org.eventhub.main.service;
 
+import org.eventhub.main.dto.EventRequest;
 import org.eventhub.main.model.Event;
 
 import java.util.List;
 
 public interface EventService {
-    Event create(Event event);
+    Event create(EventRequest event);
     Event readById(long id);
     Event update(Event event);
     void delete(long id);
--- src/main/java/org/eventhub/main/service/impl/EventServiceImpl.java
+++ src/main/java/org/eventhub/main/service/impl/EventServiceImpl.java
@@ -1,14 +1,19 @@
 package org.eventhub.main.service.impl;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.eventhub.main.dto.EventRequest;
+import org.eventhub.main.exception.NotValidDateException;
 import org.eventhub.main.exception.NullEntityReferenceException;
+import org.eventhub.main.mapper.EventDtoMapper;
 import org.eventhub.main.model.Event;
+import org.eventhub.main.model.State;
 import org.eventhub.main.repository.EventRepository;
 import org.eventhub.main.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,13 +23,46 @@ public class EventServiceImpl implements EventService {
     @Autowired
     private final EventRepository eventRepository;
 
-    public EventServiceImpl(EventRepository eventRepository) {
+    private final EventDtoMapper eventDtoMapper;
+
+    public EventServiceImpl(EventRepository eventRepository, EventDtoMapper eventDtoMapper) {
         this.eventRepository = eventRepository;
+        this.eventDtoMapper = eventDtoMapper;
     }
 
 
     @Override
-    public Event create(Event event) {
+    public Event create(EventRequest eventRequest) {
+        Event event = new Event();
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        event.setTitle(eventRequest.getTitle());
+        event.setMaxParticipants(eventRequest.getMaxParticipants());
+        event.setStartAt(eventRequest.getStartAt());
+        event.setExpireAt(eventRequest.getExpireAt());
+        event.setDescription(eventRequest.getDescription());
+        event.setLocation(eventRequest.getLocation());
+        event.setPhotos(eventRequest.getPhotos());
+        event.setOwner(eventRequest.getOwner());
+        event.setCategories(eventRequest.getCategories());
+
+        event.setCreatedAt(currentTime);
+        event.setParticipantCount(0);
+
+        int dateComparisonResultStartAt = currentTime.compareTo(eventRequest.getStartAt());
+        int dateComparisonResultExpireAt = currentTime.compareTo(eventRequest.getExpireAt());
+
+        //Exceptions
+        if (!eventRequest.getStartAt().isBefore(eventRequest.getExpireAt())) {
+            throw new NotValidDateException("Choose correct date");
+        }
+
+        if (dateComparisonResultStartAt < 0) {event.setState(State.UPCOMING);}
+        else if (dateComparisonResultStartAt >= 0 && dateComparisonResultExpireAt <= 0) {event.setState(State.LIVE);}
+        else {event.setState(State.PAST);}
+
+        event.setParticipants(new ArrayList<>());
+
         if(event != null) {
             return eventRepository.save(event);
         }
