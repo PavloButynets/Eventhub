b9cef10d318bde00f112067569b536dbf7ec4b89
= <ssaputskiy@gmail.com>
2024-02-11 18:46:37 +0200

    added notNull validations to dtos

--- src/main/java/org/eventhub/main/dto/EventRequest.java
+++ src/main/java/org/eventhub/main/dto/EventRequest.java
@@ -2,6 +2,7 @@ package org.eventhub.main.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.eventhub.main.model.Category;
 import org.eventhub.main.model.EventPhoto;
@@ -13,18 +14,24 @@ import java.util.List;
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class EventRequest {
-
+    @NotNull
     private String title;
 
+    @NotNull
     private int maxParticipants;
 
+    @NotNull
     private LocalDateTime startAt;
 
+    @NotNull
     private LocalDateTime expireAt;
 
+    @NotNull
     private String description;
 
+    @NotNull
     private String location;
 
+    @NotNull
     private long ownerId;
 }
--- src/main/java/org/eventhub/main/dto/EventResponse.java
+++ src/main/java/org/eventhub/main/dto/EventResponse.java
@@ -2,6 +2,7 @@ package org.eventhub.main.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 import org.eventhub.main.model.*;
@@ -14,25 +15,36 @@ import java.util.List;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class EventResponse {
 
+    @NotNull
     private long id;
 
+    @NotNull
     private String title;
 
+    @NotNull
     private int maxParticipants;
 
+    @NotNull
     private LocalDateTime createdAt;
 
+    @NotNull
     private LocalDateTime startAt;
 
+    @NotNull
     private LocalDateTime expireAt;
 
+    @NotNull
     private String description;
 
+    @NotNull
     private int participantCount;
 
+    @NotNull
     private State state;
 
+    @NotNull
     private String location;
 
+    @NotNull
     private long ownerId;
 }
