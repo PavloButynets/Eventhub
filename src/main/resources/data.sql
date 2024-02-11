54cad79884c0cafaf669867868b9d9ce08806a21
= <ssaputskiy@gmail.com>
2024-02-11 18:59:48 +0200

    fixed sql for new user properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 INSERT INTO categories (id, name) VALUES (1, 'Sport');
 INSERT INTO categories (id, name) VALUES (2, 'Charity');
 
-INSERT INTO users (id, first_name, last_name, username, email, password, profile_image, description, created_at, location_city, phone_number) VALUES (1, 'Nick', 'Green','nickGreen', 'nick@mail.com', '2222','image 1', 'description 1','2020-09-16 14:00:04.810221','Stryi', '0985554432');
-INSERT INTO users (id, first_name, last_name, username, email, password, profile_image, description, created_at, location_city, phone_number) VALUES (2, 'Nora', 'White','noraWhite', 'nora@mail.com', '3333','image 2', 'description 2', '2021-09-16 14:00:04.810221','Lviv', '0985553764');
-INSERT INTO users (id, first_name, last_name, username, email, password, profile_image, description, created_at, location_city, phone_number) VALUES (3, 'Mike', 'Brown','mikeBrown', 'mike@mail.com', '1111','image 3', 'description 3', '2021-10-16 14:00:04.810221','Las Vegas', '0985553964');
-INSERT INTO users (id, first_name, last_name, username, email, password, profile_image, description, created_at, location_city, phone_number) VALUES (4, 'George', 'Black','geoBlack', 'george@mail.com', '4444','image 4', 'description 4', '2021-10-16 14:00:04.810221','Berdychiv', '0995553964');
+INSERT INTO users (id, first_name, last_name, username, email, password, profile_image, description, created_at, location_city, phone_number, birth_date, gender) VALUES (1, 'Nick', 'Green','nickGreen', 'nick@mail.com', '2222','image 1', 'description 1','2020-09-16 14:00:04.810221','Stryi', '0985554432', '1990-09-11 14:00:04.810221', 'MALE');
+INSERT INTO users (id, first_name, last_name, username, email, password, profile_image, description, created_at, location_city, phone_number, birth_date, gender) VALUES (2, 'Nora', 'White','noraWhite', 'nora@mail.com', '3333','image 2', 'description 2', '2021-09-16 14:00:04.810221','Lviv', '0985553764', '1990-09-16 14:00:04.810221', 'FEMALE');
+INSERT INTO users (id, first_name, last_name, username, email, password, profile_image, description, created_at, location_city, phone_number, birth_date, gender) VALUES (3, 'Mike', 'Brown','mikeBrown', 'mike@mail.com', '1111','image 3', 'description 3', '2021-10-16 14:00:04.810221','Las Vegas', '0985553964', '1990-09-16 14:00:04.810221', 'MALE');
+INSERT INTO users (id, first_name, last_name, username, email, password, profile_image, description, created_at, location_city, phone_number, birth_date, gender) VALUES (4, 'George', 'Black','geoBlack', 'george@mail.com', '4444','image 4', 'description 4', '2021-10-16 14:00:04.810221','Berdychiv', '0995553964', '1990-09-16 14:00:04.810221', 'MALE');
 -- INSERT INTO users (id, first_name, last_name, email, password, role_id) VALUES (4, 'Mike', 'Brown', 'mike@mail.com', '$2a$10$CdEJ2PKXgUCIwU4pDQWICuiPjxb1lysoX7jrN.Y4MTMoY9pjfPALO', 1);
 --
 --
