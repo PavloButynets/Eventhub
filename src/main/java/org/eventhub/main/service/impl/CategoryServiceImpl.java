cbd1925bc9e3006fd2447dddf553e08d73e075f8
nazariusPr <protsnazar2004@gmail.com>
2024-02-12 18:26:35 +0200

    Implemented tests for Category

--- src/main/java/org/eventhub/main/service/impl/CategoryServiceImpl.java
+++ src/main/java/org/eventhub/main/service/impl/CategoryServiceImpl.java
@@ -58,7 +58,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void delete(long id) {
-        categoryRepository.deleteById(id);
+        categoryRepository.delete(readByIdEntity(id));
     }
 
     @Override
--- src/test/java/org/eventhub/main/service/CategoryServiceImplTest.java
+++ src/test/java/org/eventhub/main/service/CategoryServiceImplTest.java
@@ -3,11 +3,15 @@ package org.eventhub.main.service;
 
 import org.eventhub.main.dto.CategoryRequest;
 import org.eventhub.main.dto.CategoryResponse;
+import org.eventhub.main.exception.NullDtoReferenceException;
+import org.eventhub.main.exception.NullEntityReferenceException;
+import org.eventhub.main.model.Category;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -25,13 +29,87 @@ public class CategoryServiceImplTest {
         CategoryResponse response = categoryService.create(request);
 
         Assertions.assertNotNull(response);
-        Assertions.assertEquals(response.getName(), request.getName());
-        Assertions.assertEquals(categoryService.getAll().size(), 3);
+        Assertions.assertEquals(request.getName(),response.getName());
+        Assertions.assertEquals(3, categoryService.getAll().size());
 
         categoryService.delete(response.getId());
     }
+    @Test
+    public void createInvalidCategoryTest(){
+        Assertions.assertThrows(NullDtoReferenceException.class, () -> categoryService.create(null));
+    }
+    @Test
+    public void readByIdValidCategoryResponseTest(){
+        CategoryResponse response = categoryService.readById(10);
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals("Sport", response.getName());
+        Assertions.assertEquals(2, categoryService.getAll().size());
+    }
+    @Test
+    public void readByIdInvalidCategoryResponseTest(){
+        Assertions.assertThrows(NullEntityReferenceException.class, () -> categoryService.readById(100));
+    }
+    @Test
+    public void readByIdEntityValidCategoryTest(){
+        Category category = categoryService.readByIdEntity(10);
+        Assertions.assertNotNull(category);
+        Assertions.assertEquals("Sport", category.getName());
+        Assertions.assertEquals(2, categoryService.getAll().size());
+    }
+    @Test
+    public void readByIdEntityInvalidCategoryTest(){
+        Assertions.assertThrows(NullEntityReferenceException.class, () -> categoryService.readByIdEntity(100));
+    }
+    @Test
+    public void updateValidCategoryTest(){
+        CategoryResponse response = categoryService.create(new CategoryRequest("Tested Category"));
+        CategoryRequest request = new CategoryRequest("Updated Category");
+        CategoryResponse updatedResponse = categoryService.update(request, response.getId());
 
+        Assertions.assertNotNull(updatedResponse);
+        Assertions.assertEquals(request.getName(), updatedResponse.getName());
+        Assertions.assertEquals(3, categoryService.getAll().size());
 
+        categoryService.delete(response.getId());
+    }
+    @Test
+    void updateInvalidCategoryTest1(){
+        Assertions.assertThrows(NullDtoReferenceException.class, () -> categoryService.update(null, 10));
+    }
+    @Test
+    void updateInvalidCategoryTest2(){
+        Assertions.assertThrows(NullEntityReferenceException.class, () -> categoryService.update(new CategoryRequest("Updated name"), 100));
+    }
+    @Test
+    void deleteValidCategoryTest(){
+        CategoryRequest request1 = new CategoryRequest("Deleted Category 1");
+        CategoryResponse response1 = categoryService.create(request1);
 
+        CategoryRequest request2 = new CategoryRequest("Deleted Category 2");
+        CategoryResponse response2 = categoryService.create(request2);
+
+        Assertions.assertEquals(4, categoryService.getAll().size());
+
+        categoryService.delete(response1.getId());
+        categoryService.delete(response2.getId());
+
+        Assertions.assertEquals(2, categoryService.getAll().size());
+    }
+    @Test
+    void deleteInvalidCategoryTest(){
+        Assertions.assertThrows(NullEntityReferenceException.class, () -> categoryService.delete(100));
+    }
+    @Test
+    void getAllValidCategoryTest(){
+        CategoryResponse response = categoryService.create(new CategoryRequest("Created Category"));
+        List<CategoryResponse> all = categoryService.getAll();
+
+        Assertions.assertEquals(3, all.size());
+        Assertions.assertEquals("Sport", all.get(0).getName());
+        Assertions.assertEquals("Charity", all.get(1).getName());
+        Assertions.assertEquals("Created Category", all.get(2).getName());
+
+        categoryService.delete(response.getId());
+    }
 }
 
