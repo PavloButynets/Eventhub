efb1de5d8bf01a4c7401fc8eb0fcc8e53a2ee9f2
nazariusPr <protsnazar2004@gmail.com>
2024-02-19 21:44:32 +0200

    Changed VectorSearch

--- src/main/java/org/eventhub/main/service/impl/VectorSearchServiceImpl.java
+++ src/main/java/org/eventhub/main/service/impl/VectorSearchServiceImpl.java
@@ -4,6 +4,7 @@ import org.eventhub.main.dto.CategoryResponse;
 import org.eventhub.main.dto.EventResponse;
 import org.eventhub.main.model.State;
 import org.eventhub.main.service.CategoryService;
+import org.eventhub.main.service.EventService;
 import org.eventhub.main.service.VectorSearchService;
 import org.springframework.ai.embedding.EmbeddingClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,14 +20,13 @@ import java.util.stream.Collectors;
 public class VectorSearchServiceImpl implements VectorSearchService{
     private final EmbeddingClient embeddingClient;
     private final JdbcClient jdbcClient;
-
-    private final CategoryService categoryService;
+    private final EventService eventService;
 
     @Autowired
-    public VectorSearchServiceImpl(EmbeddingClient embeddingClient, JdbcClient jdbcClient, CategoryService categoryService) {
+    public VectorSearchServiceImpl(EmbeddingClient embeddingClient, JdbcClient jdbcClient, EventService eventService) {
         this.embeddingClient = embeddingClient;
         this.jdbcClient = jdbcClient;
-        this.categoryService = categoryService;
+        this.eventService = eventService;
     }
 
     @Override
@@ -37,16 +37,6 @@ public class VectorSearchServiceImpl implements VectorSearchService{
                 """
 SELECT
     events.id,
-    events.max_participants,
-    events.created_at,
-    events.start_at,
-    events.expire_at,
-    events.participant_count,
-    events.state,
-    events.owner_id,
-    events.title,
-    events.description,
-    events.location,
     event_embeddings.embedding
 FROM events
 INNER JOIN event_embeddings ON events.embedding_id = event_embeddings.id
@@ -54,31 +44,9 @@ WHERE 1 - (embedding <=> :user_prompt::vector) >= 0.7
 ORDER BY (embedding <=> :user_prompt::vector) LIMIT 15
 """).param("user_prompt", embedding.toString());
 
-
         return query.query((resultSet, statementContext) -> {
-                    EventResponse newEvent = new EventResponse();
-
-                    try {
-                        newEvent.setId(resultSet.getLong("id"));
-                        newEvent.setTitle(resultSet.getString("title"));
-                        newEvent.setMaxParticipants(resultSet.getInt("max_participants"));
-                        newEvent.setCreatedAt(resultSet.getObject("created_at", LocalDateTime.class));
-                        newEvent.setStartAt(resultSet.getObject("start_at", LocalDateTime.class));
-                        newEvent.setExpireAt(resultSet.getObject("expire_at", LocalDateTime.class));
-                        newEvent.setDescription(resultSet.getString("description"));
-                        newEvent.setParticipantCount(resultSet.getInt("participant_count"));
-                        newEvent.setState(State.valueOf(resultSet.getString("state")));
-                        newEvent.setLocation(resultSet.getString("location"));
-                        newEvent.setOwnerId(resultSet.getLong("owner_id"));
-                        newEvent.setCategoryResponses(new ArrayList<>());
-
-                        newEvent.getCategoryResponses().addAll(categoryService.getAllByEventId(resultSet.getLong("id")));
-                    }
-                    catch (SQLException ex){
-                        throw new RuntimeException(ex.getMessage());
-                    }
-
-                return newEvent;
+            long id = resultSet.getLong("id");
+            return eventService.readById(id);
         }).list();
     }
 }
