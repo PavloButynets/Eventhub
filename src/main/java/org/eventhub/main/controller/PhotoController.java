01eaf31b962fe5b1730ffe24b41c9df74dc664bb
nazariusPr <protsnazar2004@gmail.com>
2024-02-20 17:36:50 +0200

    Implemented storage in azure

--- src/main/java/org/eventhub/main/controller/PhotoController.java
+++ src/main/java/org/eventhub/main/controller/PhotoController.java
@@ -1,5 +1,10 @@
 package org.eventhub.main.controller;
 
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobClientBuilder;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
+import com.azure.storage.blob.specialized.BlockBlobClient;
 import lombok.extern.slf4j.Slf4j;
 import org.eventhub.main.dto.*;
 import org.eventhub.main.exception.ResponseStatusException;
@@ -12,13 +17,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping("/users/{user_id}/events/{event_id}/photos")
 public class PhotoController {
+    private static final String connectionString = "DefaultEndpointsProtocol=https;AccountName=eventhubproject;AccountKey=KDM2AysSXKu3dXoifZKlsp0/D3Fz4APx6ySAGEJeFtLpQ/AZG3UhG0wUyIscSPXXVsWjFVYYWbiP+ASt1bCrLw==;EndpointSuffix=core.windows.net";
+    private static final String containerName = "images";
     private final PhotoService photoService;
 
     @Autowired
@@ -58,11 +72,45 @@ public class PhotoController {
     }
 
     @DeleteMapping("/{photo_id}")
-    public ResponseEntity<OperationResponse> delete(@PathVariable("photo_id") long photoId) {
-        String name = photoService.readById(photoId).getPhotoUrl();
+    public ResponseEntity<OperationResponse> delete(@PathVariable("photo_id") long photoId) throws UnsupportedEncodingException {
+        String blobUrl = photoService.readById(photoId).getPhotoUrl();
+        BlobContainerClient blobContainerClient = new BlobContainerClientBuilder()
+                .connectionString(connectionString)
+                .containerName(containerName)
+                .buildClient();
+        BlockBlobClient blockBlobClient = blobContainerClient.getBlobClient(blobUrl).getBlockBlobClient();
+
+        blockBlobClient.delete();
+
         log.info("**/deleted user(id) = " + photoId);
         photoService.delete(photoId);
-        return new ResponseEntity<>(new OperationResponse("EventPhoto " + name + " deleted successfully"), HttpStatus.OK);
+        return new ResponseEntity<>(new OperationResponse("EventPhoto " + blobUrl + " deleted successfully"), HttpStatus.OK);
     }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImages(@PathVariable(name = "event_id") Long eventId,
+                                               @RequestPart("files") List<MultipartFile> files) {
+        try {
+            BlobContainerClient blobContainerClient = new BlobContainerClientBuilder()
+                    .connectionString(connectionString)
+                    .containerName(containerName)
+                    .buildClient();
+
+            for (MultipartFile file : files) {
+                try (ByteArrayInputStream dataStream = new ByteArrayInputStream(file.getBytes())) {
+                    BlockBlobClient blockBlobClient = blobContainerClient.getBlobClient(file.getOriginalFilename()).getBlockBlobClient();
+                    blockBlobClient.upload(dataStream, file.getSize());
+
+                    String imageUrl = blockBlobClient.getBlobName();
+                    photoService.create(new EventPhotoRequest(imageUrl, eventId));
+                }
+            }
+            return ResponseEntity.ok("Images uploaded successfully.");
+        } catch (IOException e) {
+            log.error("Error uploading images.", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error uploading images.");
+        }
+    }
+
 }
 
