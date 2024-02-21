97b136b060ef1a02bb15df6b7c7a2a5f13720a8a
viktor <viktorpehun@gmail.com>
2024-02-21 22:40:41 +0200

    Added social media login buttons

--- event_hub_client/src/pages/LogIn/LogIn.jsx
+++ event_hub_client/src/pages/LogIn/LogIn.jsx
@@ -1,75 +1,121 @@
 import React from 'react';
+import axios from 'axios';
 import styles from './LogIn.module.css';
+import { Link } from 'react-router-dom';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
 const LogIn = () => {
-    const onFinish = (values) => {
-        console.log('Received values of form: ', values);
-    };
+    // const onFinish = async (values) => {
+    //     try {
+    //         const response = await axios.post('http://localhost:3001/users', {
+    //             email: values.email,
+    //             password: values.password
+    //         });
+    //         console.log(response.data); // Assuming your server responds with user data upon successful login
+    //         // Redirect the user to the dashboard or perform any other necessary actions upon successful login
+    //     } catch (error) {
+    //         console.error('Login failed:', error);
+    //         // Handle login failure, e.g., show error message to the user
+    //     }
+    // };
+
+
+
+
     return (
 
-        <div className={styles.container}>
-            <img className={styles.Photo} src="/images/EventHub_loginPhoto.jpg" alt="Your Photo"/>
+        <div className={styles.bodyC}>
+            <div className={styles.container}>
+                {/*<img className={styles.Photo} src="/images/EventHub_loginPhoto.jpg" alt="Your Photo"/>*/}
 
 
-            <Form
+                <Form
 
-                name="normal_login"
-                className="login-form"
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-            >
-                <h1>Login</h1>
-                <Form.Item
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your Username!',
-                        },
-                    ]}
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
                 >
-                    <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Username"/>
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your Password!',
-                        },
-                    ]}
-                >
-                    <Input.Password
-                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                    <h1>Login</h1>
+                    <Form.Item
+                        name="email"
+                        rules={[
+                            {
 
-                        type="password"
-                        placeholder="Password"
-                    />
-                </Form.Item>
+                                required: true,
+                                message: 'Please input your email!',
+                            },
+                        ]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Email"/>
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password
+                            prefix={<LockOutlined className="site-form-item-icon"/>}
 
-                <Form.Item>
-                    <div className={styles.RememberMe}>
-                        <Form.Item name="remember" valuePropName="checked" noStyle>
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
 
-                        <a className="login-form-forgot" href="">
-                            Forgot password
-                        </a>
-                    </div>
-                </Form.Item>
+                    <Form.Item>
+                        <div className={styles.RememberMe}>
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+
+                            <a className="login-form-forgot" href="">
+                                Forgot password
+                            </a>
+                        </div>
+                    </Form.Item>
 
 
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className={styles.loginButton}>
-                        LogIn
-                    </Button>
-                    <div>Or <a href="">register now!</a></div>
-                </Form.Item>
-            </Form>
+                    <Form.Item style={{marginBottom: "0px"}}>
+                        <Button type="primary" htmlType="submit" className={styles.loginButton}>
+                            Login
+                        </Button>
+
+                    </Form.Item>
+                    <p style={{textAlign: "center"}}>Or</p>
+
+
+                    <Form.Item style={{marginBottom: "0px"}}>
+                        <div className={styles.loginButtonContainer}>
+
+                            <Button type="link" htmlType="button" className={styles.socialMediaLogin}>
+                                <img className={styles.loginImg} src="/images/fb_logo.png"
+                                     alt="Continue with Facebook"/>
+                            </Button>
+
+
+                            <Button type="link" htmlType="button" className={styles.socialMediaLogin}>
+                                <img className={styles.loginImg} src="/images/apple_logo.png"
+                                     alt="Continue with Apple"/>
+                            </Button>
+
+
+                            <Button type="link" htmlType="button" className={styles.socialMediaLogin}>
+                                <img className={styles.loginImg} src="/images/google_logo.png"
+                                     alt="Continue with Google"/>
+                            </Button>
+                        </div>
+
+                    </Form.Item>
+                    <p style={{textAlign: "center", fontSize:"12px"}}>Don’t have an account in EventHub yet? <a href="/register">Register!</a></p>
+
+                </Form>
+            </div>
         </div>
     );
 };
