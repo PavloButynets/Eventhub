aa6584a48b3910ec7019f6325c7c277f18c0f25d
Stas Topilnytskiy <stastopilnytskii@gmail.com>
2024-03-14 17:54:41 +0200

    added register controller

--- src/main/java/org/eventhub/main/config/SecurityConfig.java
+++ src/main/java/org/eventhub/main/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/authentication/login")
+                .requestMatchers("/authentication/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
