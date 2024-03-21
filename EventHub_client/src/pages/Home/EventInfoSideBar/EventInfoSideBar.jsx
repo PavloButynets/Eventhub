eeed509c49fe02bbaf5547d64a3183201b066c13
= <ssaputskiy@gmail.com>
2024-03-21 11:14:52 +0200

    Added date without styles

--- EventHub_client/src/pages/Home/EventInfoSideBar/EventInfoSideBar.jsx
+++ EventHub_client/src/pages/Home/EventInfoSideBar/EventInfoSideBar.jsx
@@ -2,10 +2,25 @@ import { useState } from 'react';
 import styles from './EventInfoSideBar.module.css'
 import { SlArrowLeft } from "react-icons/sl";
 import { SlArrowRight } from "react-icons/sl";
+import { MdOutlineDateRange } from "react-icons/md";
 
 const EventInfoSideBar = ({event}) => {
 
     const [photoIndex, setPhotoIndex] = useState(0);
+
+    const month = new Map();
+    month.set('01', 'Jan');
+    month.set('02', 'Feb');
+    month.set('03', 'Mar');
+    month.set('04', 'Apr');
+    month.set('05', 'May');
+    month.set('06', 'Jun');
+    month.set('07', 'Jul');
+    month.set('08', 'Aug');
+    month.set('09', 'Sep');
+    month.set('10', 'Nov');
+    month.set('11', 'Oct');
+    month.set('12', 'Dec');
 
     const handleRightPhotoClick = () => {
         if (photoIndex < event.photo_responses.length-1) {
@@ -41,7 +56,26 @@ const EventInfoSideBar = ({event}) => {
             </div>
 
             <div className={styles['date-container']}>
-                
+                <div className={styles['date-range-container']}>
+                    <div className={styles['start-at']}>
+                        <div className={styles['day']}>
+                            {month.get(event.start_at.slice(5,7)) + ' ' + event.start_at.slice(8,10)}
+                        </div>
+                        <div className={styles['time']}>
+                            {event.start_at.slice(11,16)}
+                        </div>
+                    </div>
+
+                    <div className={styles['expire-at']}>
+                        <div className={styles['day']}>
+                            {month.get(event.expire_at.slice(5,7)) + ' ' + event.expire_at.slice(8,10)}
+                        </div>
+                        <div className={styles['time']}>
+                            {event.expire_at.slice(11,16)}
+                        </div>
+                    </div>
+                </div>
+                <MdOutlineDateRange />
             </div>
         </div>
      );
