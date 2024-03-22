51de4954099e96590eff4cfb8ab602d3ba39283f
= <ssaputskiy@gmail.com>
2024-03-22 13:22:45 +0200

    Added participantPhotoResponse to mapper

--- src/main/java/org/eventhub/main/mapper/ParticipantMapper.java
+++ src/main/java/org/eventhub/main/mapper/ParticipantMapper.java
@@ -2,11 +2,13 @@ package org.eventhub.main.mapper;
 
 import org.eventhub.main.dto.ParticipantRequest;
 import org.eventhub.main.dto.ParticipantResponse;
+import org.eventhub.main.dto.ParticipantWithPhotoResponse;
 import org.eventhub.main.exception.NullDtoReferenceException;
 import org.eventhub.main.exception.NullEntityReferenceException;
 import org.eventhub.main.model.Participant;
 import org.eventhub.main.repository.EventRepository;
 import org.eventhub.main.repository.UserRepository;
+import org.eventhub.main.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +18,13 @@ public class ParticipantMapper {
     private final UserRepository userRepository;
     private final EventRepository eventRepository;
 
+    private final UserService userService;
+
     @Autowired
-    public ParticipantMapper(UserRepository userRepository, EventRepository eventRepository){
+    public ParticipantMapper(UserRepository userRepository, EventRepository eventRepository, UserService userService){
         this.userRepository = userRepository;
         this.eventRepository = eventRepository;
+        this.userService = userService;
     }
     public ParticipantResponse entityToResponse(Participant participant) {
         if (participant == null) {
@@ -45,5 +50,14 @@ public class ParticipantMapper {
         participant.setUser(userRepository.findById(participantRequest.getUserId()).orElseThrow(()->new NullEntityReferenceException("User cant be null")));
         participant.setEvent(eventRepository.findById(participantRequest.getEventId()).orElseThrow(()->new NullEntityReferenceException("Event cant be null")));
         return participant;
+    }
+
+    public ParticipantWithPhotoResponse responseToPhotoResponse(ParticipantResponse participantResponse) {
+
+        return ParticipantWithPhotoResponse.builder()
+                .id(participantResponse.getId())
+                .userId(participantResponse.getUserId())
+                .photoUrl(userService.readById(participantResponse.getUserId()).getPhotoResponses().get(0).getPhotoUrl())
+                .build();
     }
 }
