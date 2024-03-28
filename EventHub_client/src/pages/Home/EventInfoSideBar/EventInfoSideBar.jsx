80b9473a928eb9afac24ffcb5d8f68c721dd9e1d
= <ssaputskiy@gmail.com>
2024-03-28 09:29:24 +0200

    Fixed event photo section

--- EventHub_client/src/pages/Home/EventInfoSideBar/EventInfoSideBar.jsx
+++ EventHub_client/src/pages/Home/EventInfoSideBar/EventInfoSideBar.jsx
@@ -98,17 +98,18 @@ const EventInfoSideBar = ({event}) => {
             <h2 className={styles['event-title']}>side-bar-container</h2>
 
             {/* Photo */}
-            {
-                (event.photo_responses.length !== 0) ? 
-                (<div className={styles['photo-container']}>
+            
+                
+                <div className={styles['photo-container']}>
                     <img className={styles['event-photo']} src={event.photo_responses[photoIndex].photo_url} alt="Event img" />
-                    <div className={styles['arrow-container']}>
+                    {event.photo_responses.length > 1 && <div className={styles['arrow-container']}>
                         <button onClick={handleLeftPhotoClick}> <SlArrowLeft /> </button>
                         <button onClick={handleRightPhotoClick}> <SlArrowRight /> </button>
-                    </div>
-                </div>) :
-                (<img className={styles['event-photo']} src='https://eventhub12.blob.core.windows.net/images/default.jpg?sp=r&st=2024-03-18T06:52:24Z&se=2024-03-24T14:52:24Z&spr=https&sv=2022-11-02&sr=b&sig=nWb0Dzb9%2FWPfAZ6X5MRrwoi%2FxHU8OLe0I6nPtwpBkbQ%3D' alt='Event img' />)
-            }
+                    </div>}
+                 </div>
+                 
+                {/*(<img className={styles['event-photo']} src={event.photo_responses[0].photo_url} alt='Event img' />) */}
+            
 
             {/* Category */}
             <div className={styles['category-container']}>
