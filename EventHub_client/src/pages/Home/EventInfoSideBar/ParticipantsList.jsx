bc61e887f03fa4633843b7391f3760fb8a42939e
= <ssaputskiy@gmail.com>
2024-03-29 12:14:25 +0200

    Finished basic styling accept sizing of participants list

--- EventHub_client/src/pages/Home/EventInfoSideBar/ParticipantsList.jsx
+++ EventHub_client/src/pages/Home/EventInfoSideBar/ParticipantsList.jsx
@@ -16,25 +16,25 @@ const ParticipantsList = ({event, handleGoBackToSideBar, handleCloseWindow}) => 
     useEffect(() => {
         getUserParticipants(event.id)
         .then(data => setParticipants(data));
-    });
+    }, [event]);
 
     return ( 
         <div className={styles['participants-list-container']}>
             <div className={styles['header']}>
-                <button onClick={handleGoBackToSideBar}><IoArrowBackOutline /></button>
-                <button onClick={handleCloseWindow}><IoClose /></button>
+                <button className={styles['btn']} onClick={handleGoBackToSideBar}><IoArrowBackOutline className={styles['btn-icon']} /></button>
+                <button className={styles['btn']} onClick={handleCloseWindow}><IoClose className={styles['btn-icon']} /></button>
             </div>
 
             <div className={styles['participants-container']}>
                 {participants.map(participant => (
-                    <div className={styles['participant-container']}>
-                        <img src={participant.participant_photo.photo_url} alt="User participant img" />
+                    <div key={participant.id} className={styles['participant-container']}>
+                        <img className={styles['participant-photo']} src={participant.participant_photo.photo_url} alt="User participant img" />
                         <div className={styles['participant-info-container']}>
                             <div className={styles['full-name']}>
                                 <p>{participant.first_name}</p>
                                 <p>{participant.last_name}</p>
                             </div>
-                            <p>{participant.email}</p>
+                            <p className={styles['email']}>{participant.email}</p>
                         </div>
                     </div>
                 ))}
