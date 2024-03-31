7f961de989b9b39b7b11e4dddd1650fc61924b61
Yaroslav <ssaputskyy@gmail.com>
2024-03-31 18:17:59 +0300

    Started working on animations and formated code

--- EventHub_client/src/pages/Home/EventInfoSideBar/EventInfoSideBar.jsx
+++ EventHub_client/src/pages/Home/EventInfoSideBar/EventInfoSideBar.jsx
@@ -1,213 +1,253 @@
-import { useState, useEffect, useRef } from 'react';
-import styles from './EventInfoSideBar.module.css'
+import { useState, useEffect, useRef } from "react";
+import styles from "./EventInfoSideBar.module.css";
 import { IoIosMore } from "react-icons/io";
-import {getParticipants} from '../../../api/getParticipants';
-import {getUserById} from '../../../api/getUserById';
+import { getParticipants } from "../../../api/getParticipants";
+import { getUserById } from "../../../api/getUserById";
 import { RiVipCrownLine } from "react-icons/ri";
 import { IoClose } from "react-icons/io5";
-import ImageSlider from '../../../components/ImageSlider/ImageSlider';
-import { useNavigate, useParams } from 'react-router-dom';
-import { getEventById } from '../../../api/getEventById';
-import ParticipantsList from './ParticipantsList';
-
+import ImageSlider from "../../../components/ImageSlider/ImageSlider";
+import { useNavigate, useParams } from "react-router-dom";
+import { getEventById } from "../../../api/getEventById";
+import ParticipantsList from "./ParticipantsList";
+import { AnimatePresence, motion } from "framer-motion";
 
 const EventInfoSideBar = () => {
+  const [isShowMore, setIsShowMore] = useState(false);
+  const [isOverflowAboutText, setIsOverflowAboutText] = useState(false);
+  const [isShowMoreParticipants, setIsShowMoreParticipants] = useState(false);
+  const [participantsToShow, setParticipantsToShow] = useState([]);
+  const [event, setEvent] = useState(null);
 
-    const [isShowMore, setIsShowMore] = useState(false);
-    const [isOverflowAboutText, setIsOverflowAboutText] = useState(false);
-    const [isShowMoreParticipants, setIsShowMoreParticipants] = useState(false);
-    const [participantsToShow, setParticipantsToShow] = useState([]);
-    const [event, setEvent] = useState(null);
-    
-    const [owner, setOwner] = useState(null);
+  const [owner, setOwner] = useState(null);
 
-    const [showAllParticipants, setShowAllParticipants] = useState(false);
+  const [showAllParticipants, setShowAllParticipants] = useState(false);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleShowAllParticipants = () => {
-        setShowAllParticipants(prev => !prev);
-        
-    }
+  const handleShowAllParticipants = () => {
+    setShowAllParticipants((prev) => !prev);
+  };
 
-    const handleCloseWindow = () => {
-        navigate('../');
-    }
+  const handleCloseWindow = () => {
+    navigate("../");
+  };
 
-    const sideBar = useRef(null);
-    const showMoreBtn = useRef(null);
-    const aboutText = useRef(null);
+  const sideBar = useRef(null);
+  const showMoreBtn = useRef(null);
+  const aboutText = useRef(null);
 
-    const {ownerId, eventId} = useParams();
+  const { ownerId, eventId } = useParams();
 
-    useEffect(() => {
-        getEventById(ownerId, eventId)
-        
-        .then(data => {
-            
-            setEvent(data);
-        })
-    }, [ownerId, eventId]);
-    
-    useEffect(() => {
-        event && getParticipants(event.id)
-        .then(data => {
-            console.log('Data: ',data);
-            if (data.length > 1) {
-                setIsShowMoreParticipants(true);
-                setParticipantsToShow(data.slice(0,4));
-            }
-            else {
-                setParticipantsToShow(data);
-            }
+  useEffect(() => {
+    getEventById(ownerId, eventId).then((data) => {
+      setEvent(data);
+    });
+  }, [ownerId, eventId]);
 
-            if (aboutText.current.scrollHeight > aboutText.current.clientHeight) {
-                setIsOverflowAboutText(true);
-            }
-        });
+  useEffect(() => {
+    event &&
+      getParticipants(event.id).then((data) => {
+        console.log("Data: ", data);
+        if (data.length > 1) {
+          setIsShowMoreParticipants(true);
+          setParticipantsToShow(data.slice(0, 4));
+        } else {
+          setParticipantsToShow(data);
+        }
 
-        return () => setIsShowMoreParticipants(false);
-        
-        
-    }, [event]);
+        if (aboutText.current.scrollHeight > aboutText.current.clientHeight) {
+          setIsOverflowAboutText(true);
+        }
+      });
 
-    useEffect(() => {
-        event && getUserById(event.owner_id)
-        .then(data => {
-            setOwner(data);
-            console.log(`Owner photo response: ${data.photo_responses[0].photo_url}`)
-        })
-    }, [event])
+    return () => setIsShowMoreParticipants(false);
+  }, [event]);
 
-    useEffect(() => {
-        console.log('Show All: ',showAllParticipants);
-        event && ((showAllParticipants) ? sideBar.current.style.opacity = 0 : sideBar.current.style.opacity = 1);
-    }, [showAllParticipants, event]);
+  useEffect(() => {
+    event &&
+      getUserById(event.owner_id).then((data) => {
+        setOwner(data);
+        console.log(
+          `Owner photo response: ${data.photo_responses[0].photo_url}`
+        );
+      });
+  }, [event]);
 
+  useEffect(() => {
+    console.log("Show All: ", showAllParticipants);
+    event &&
+      (showAllParticipants
+        ? (sideBar.current.style.opacity = 0)
+        : (sideBar.current.style.opacity = 1));
+  }, [showAllParticipants, event]);
 
-    const month = new Map();
-    month.set('01', 'Jan');
-    month.set('02', 'Feb');
-    month.set('03', 'Mar');
-    month.set('04', 'Apr');
-    month.set('05', 'May');
-    month.set('06', 'Jun');
-    month.set('07', 'Jul');
-    month.set('08', 'Aug');
-    month.set('09', 'Sep');
-    month.set('10', 'Nov');
-    month.set('11', 'Oct');
-    month.set('12', 'Dec');
+  const month = new Map();
+  month.set("01", "Jan");
+  month.set("02", "Feb");
+  month.set("03", "Mar");
+  month.set("04", "Apr");
+  month.set("05", "May");
+  month.set("06", "Jun");
+  month.set("07", "Jul");
+  month.set("08", "Aug");
+  month.set("09", "Sep");
+  month.set("10", "Nov");
+  month.set("11", "Oct");
+  month.set("12", "Dec");
 
-    
+  const handleShowMore = () => {
+    setIsShowMore((prev) => !prev);
+    showMoreBtn.current.innerHTML = isShowMore ? "Show more" : "Show less";
+  };
 
-    const handleShowMore = () => {
-        setIsShowMore(prev => !prev);
-        showMoreBtn.current.innerHTML = isShowMore ? 'Show more' : 'Show less';
-    }
-
-    
-
-    return ( 
-        event && <div>
-                    <div className={styles['side-bar-container']} ref={sideBar}>
-                        <div className={styles['header']}>
-                            <h2 className={styles['event-title']}>{event.title}</h2>
-                            <button className={styles['close-btn']} onClick={handleCloseWindow}><IoClose className={styles['close-btn-icon']} size="2.5em" /></button>
-                        </div>
-                        
-
-                        {/* Photo */}
-                        <div className={styles['photo-container']}>
-                            <ImageSlider images={event.photo_responses}/>
-                        </div>
-                        
-
-                        {/* Category */}
-                        <div className={styles['category-container']}>
-                            {event.category_responses.map((category) => (
-                                <div key={category.id} className={styles['category']}>{category.name}</div>
-                            ))}
-                        </div>
-
-                        {/* Date */}
-                        <h3 className={styles['heading']}>Date and time</h3>
-                        <div className={styles['date-container']}>
-                            
-                            <div className={styles['date-range-container']}>
-                                <div className={styles['start-at']}>
-                                    <div className={styles['day']}>
-                                        {month.get(event.start_at.slice(5,7)) + ' ' + event.start_at.slice(8,10)}
-                                    </div>
-                                    <div className={styles['time']}>
-                                        {event.start_at.slice(11,16)}
-                                    </div>
-                                </div>
-                                
-
-                                <div className={styles['expire-at']}>
-                                    <div className={styles['day']}>
-                                        {month.get(event.expire_at.slice(5,7)) + ' ' + event.expire_at.slice(8,10)}
-                                    </div>
-                                    <div className={styles['time']}>
-                                        {event.expire_at.slice(11,16)}
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className={styles['vl']}></div>
-                            <div className={styles['location']}>
-                                {event.location}
-                            </div>
-                            
-                        </div>
-
-                        {/* Participants */}
-                        <h3 className={styles['heading']}>Participants</h3>
-                        <div className={styles['participant-container']}>
-                            
-                            <div className={styles['participants-photos']}>
-                                <div className={styles['owner-photo']}>
-                                    {owner && <img src={owner.photo_responses[0].photo_url} alt="" />}
-                                    <div className={styles['crown-container']}>
-                                        <RiVipCrownLine className={styles['crown-icon']} />
-                                    </div>
-                                    
-                                </div>
-                                {participantsToShow.map(participant => (
-                                    <div className={styles['item']} key={participant.id}>
-                                        <img src={participant.participant_photo.photo_url} alt="Participant Img" />
-                                    </div>
-                                ))}    
-                                { isShowMoreParticipants && <div className={styles['show-more-participants']}><button onClick={handleShowAllParticipants} className={styles['show-more-participants-btn']} ><IoIosMore className={styles['show-more-participants-btn-icon']} /></button></div>}
-                            </div>
-                            
-                        </div>
-
-                        {/* About section */}
-                        <h3 className={styles['heading']}>About this event</h3>
-                        <div className={styles['about-container']}>
-                            <div className={styles[isShowMore ? null : 'about-text']} ref={aboutText}>
-                                {event.description}
-                            </div>
-                            {isOverflowAboutText &&  <button onClick={handleShowMore} className={styles['show-more-btn']} ref={showMoreBtn}>Show more</button>}
-                        </div>
-
-                        {/* Lower section */}
-                        <div className={styles['lower-container']}>
-                            <div className={styles['spots']}>
-                                    {event.max_participants - event.participant_count} Spots left
-                            </div>
-
-                            <button className={styles['action-btn']}>Action</button>
-                        </div>
-
-                    
-                    
-                </div>
-                {event && showAllParticipants && <ParticipantsList event={event} handleGoBackToSideBar={handleShowAllParticipants} handleCloseWindow={handleCloseWindow} />}
+  return (
+    <AnimatePresence>
+      {event && (
+        <motion.div
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+          }}
+          exit={{
+            opacity: 0,
+          }}
+          transition={{
+            duration: 1,
+          }}
+        >
+          <div className={styles["side-bar-container"]} ref={sideBar}>
+            <div className={styles["header"]}>
+              <h2 className={styles["event-title"]}>{event.title}</h2>
+              <button
+                className={styles["close-btn"]}
+                onClick={handleCloseWindow}
+              >
+                <IoClose className={styles["close-btn-icon"]} size="2.5em" />
+              </button>
             </div>
-     );
-}
- 
+
+            {/* Photo */}
+            <div className={styles["photo-container"]}>
+              <ImageSlider images={event.photo_responses} />
+            </div>
+
+            {/* Category */}
+            <div className={styles["category-container"]}>
+              {event.category_responses.map((category) => (
+                <div key={category.id} className={styles["category"]}>
+                  {category.name}
+                </div>
+              ))}
+            </div>
+
+            {/* Date */}
+            <h3 className={styles["heading"]}>Date and time</h3>
+            <div className={styles["date-container"]}>
+              <div className={styles["date-range-container"]}>
+                <div className={styles["start-at"]}>
+                  <div className={styles["day"]}>
+                    {month.get(event.start_at.slice(5, 7)) +
+                      " " +
+                      event.start_at.slice(8, 10)}
+                  </div>
+                  <div className={styles["time"]}>
+                    {event.start_at.slice(11, 16)}
+                  </div>
+                </div>
+
+                <div className={styles["expire-at"]}>
+                  <div className={styles["day"]}>
+                    {month.get(event.expire_at.slice(5, 7)) +
+                      " " +
+                      event.expire_at.slice(8, 10)}
+                  </div>
+                  <div className={styles["time"]}>
+                    {event.expire_at.slice(11, 16)}
+                  </div>
+                </div>
+              </div>
+
+              <div className={styles["vl"]}></div>
+              <div className={styles["location"]}>{event.location}</div>
+            </div>
+
+            {/* Participants */}
+            <h3 className={styles["heading"]}>Participants</h3>
+            <div className={styles["participant-container"]}>
+              <div className={styles["participants-photos"]}>
+                <div className={styles["owner-photo"]}>
+                  {owner && (
+                    <img src={owner.photo_responses[0].photo_url} alt="" />
+                  )}
+                  <div className={styles["crown-container"]}>
+                    <RiVipCrownLine className={styles["crown-icon"]} />
+                  </div>
+                </div>
+                {participantsToShow.map((participant) => (
+                  <div className={styles["item"]} key={participant.id}>
+                    <img
+                      src={participant.participant_photo.photo_url}
+                      alt="Participant Img"
+                    />
+                  </div>
+                ))}
+                {isShowMoreParticipants && (
+                  <div className={styles["show-more-participants"]}>
+                    <button
+                      onClick={handleShowAllParticipants}
+                      className={styles["show-more-participants-btn"]}
+                    >
+                      <IoIosMore
+                        className={styles["show-more-participants-btn-icon"]}
+                      />
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+
+            {/* About section */}
+            <h3 className={styles["heading"]}>About this event</h3>
+            <div className={styles["about-container"]}>
+              <div
+                className={styles[isShowMore ? null : "about-text"]}
+                ref={aboutText}
+              >
+                {event.description}
+              </div>
+              {isOverflowAboutText && (
+                <button
+                  onClick={handleShowMore}
+                  className={styles["show-more-btn"]}
+                  ref={showMoreBtn}
+                >
+                  Show more
+                </button>
+              )}
+            </div>
+
+            {/* Lower section */}
+            <div className={styles["lower-container"]}>
+              <div className={styles["spots"]}>
+                {event.max_participants - event.participant_count} Spots left
+              </div>
+
+              <button className={styles["action-btn"]}>Action</button>
+            </div>
+          </div>
+          {event && showAllParticipants && (
+            <ParticipantsList
+              event={event}
+              handleGoBackToSideBar={handleShowAllParticipants}
+              handleCloseWindow={handleCloseWindow}
+            />
+          )}
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
 export default EventInfoSideBar;
