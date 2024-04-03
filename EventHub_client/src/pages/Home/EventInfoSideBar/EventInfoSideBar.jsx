436de70e75ccf328719b55c30a6082809b76a6fe
= <ssaputskiy@gmail.com>
2024-04-03 15:18:45 +0300

    Moved moth to separate folder utils

--- EventHub_client/src/pages/Home/EventInfoSideBar/EventInfoSideBar.jsx
+++ EventHub_client/src/pages/Home/EventInfoSideBar/EventInfoSideBar.jsx
@@ -12,6 +12,8 @@ import ImageSlider from "../../../components/ImageSlider/ImageSlider";
 import CloseWindowButton from "../../../components/Buttons/CloseWindowButton/CloseWindowButton";
 import ParticipantsList from "./ParticipantsList";
 
+import month from "../../../utils/month";
+
 import { AnimatePresence, motion } from "framer-motion";
 import ParticipantInfoPopUp from "../../../components/PopUp/ParticipantInfoPopUp";
 import PrimaryButton from "../../../components/Buttons/PrimaryButton/PrimaryButton";
@@ -92,20 +94,6 @@ const EventInfoSideBar = ({ ownerId, eventId }) => {
   //       ? (sideBar.current.style.opacity = 0)
   //       : (sideBar.current.style.opacity = 1));
   // }, [showAllParticipants, event]);
-
-  const month = new Map();
-  month.set("01", "Jan");
-  month.set("02", "Feb");
-  month.set("03", "Mar");
-  month.set("04", "Apr");
-  month.set("05", "May");
-  month.set("06", "Jun");
-  month.set("07", "Jul");
-  month.set("08", "Aug");
-  month.set("09", "Sep");
-  month.set("10", "Nov");
-  month.set("11", "Oct");
-  month.set("12", "Dec");
 
   // Funcs
   const handleShowAllParticipants = () => {
