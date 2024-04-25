82f723c60f2acff77117bea03964e65cfc088fda
nazariusPr <115506158+nazariusPr@users.noreply.github.com>
2024-04-25 20:17:45 +0300

    fix bug with custom hook

--- EventHub_client/src/hooks/useLogin.js
+++ EventHub_client/src/hooks/useLogin.js
@@ -1,11 +1,14 @@
 import { useState, useEffect, useContext } from "react";
 import AuthContext from "../context/authProvider";
+import useAuth from "./useAuth";
 
 const useLogin = () => {
-  const [authenticated, setAuthenticated] = useState(!!localStorage.getItem("token"));
+  const [authenticated, setAuthenticated] = useState(
+    !!localStorage.getItem("token")
+  );
 
   useEffect(() => {
-    setAuthenticated(!authenticated);
+    setAuthenticated(!!localStorage.getItem("token"));
   }, [localStorage.getItem("token")]);
 
   return authenticated;
